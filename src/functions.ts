8d9afe5423144e571a048ffe8d858da8e199f357
THIAGO <thiagohenssuchi@gmail.com>
2024-11-06 17:46:51 -0400

    Type in functions - lambda and void

--- src/functions.ts
+++ src/functions.ts
@@ -8,3 +8,22 @@ console.log(result);
 
 // função lambda ou função de seta
 const soma2: (x: number, y: number) => number = (x, y) => x + y;
+
+
+// Função com void
+function semRetorno(...args: string[]): void {
+    console.log(args.join(' '));
+}
+
+semRetorno('Thiago', 'Augusto');
+
+const person2 = {
+    nome: 'Luís',
+    sobrenome: 'Olávo',
+
+    exibirNome(): void {
+        console.log(this.nome + ' ' + this.sobrenome);
+    }
+}
+
+person2.exibirNome()
