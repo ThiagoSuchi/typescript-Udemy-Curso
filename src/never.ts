00c83061e2d4ecae7b51d05a5eb4bd51e2826f14
THIAGO <thiagohenssuchi@gmail.com>
2024-11-08 13:53:02 -0400

    Type never

--- src/never.ts
+++ src/never.ts
@@ -1,3 +1,4 @@
+// Exemplo de uma function que não retorna nada a não ser um error.
 export function criarError(): never {
     throw new Error('Erro qualquer');
 }
@@ -5,3 +6,22 @@ export function criarError(): never {
 criarError();
 
 // O never é um tipo que indica "ausência de valor" e é utilizado principalmente em funções que não retornam (como exceções) e em verificações de tipos para garantir segurança em tempo de compilação.
+
+type Status = "pendente" | "concluido" | "cancelado";
+
+function checarStatus(status: Status): string {
+    switch (status) {
+        case "pendente":
+            return "O compromisso está pendente.";
+        case "concluido":
+            return "O compromisso foi concluído.";
+        case "cancelado":
+            return "O compromisso foi cancelado.";
+        default:
+             // Erro em tempo de compilação se `status` for um valor inesperado
+            const verificaNever: never = status;
+            return verificaNever;
+    }
+}
+
+console.log(checarStatus('cancelado'));
