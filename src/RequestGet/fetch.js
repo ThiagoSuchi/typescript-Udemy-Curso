4396c398efb45dc6db2ed539b4ae306e391b2a59
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2025-02-03 13:48:13 -0400

    Fetch API - promises async await - try catch...

--- src/RequestGet/fetch.js
+++ src/RequestGet/fetch.js
@@ -26,16 +26,14 @@ document.addEventListener('click', (e) => {
 })
 
 async function carregaPagina(element) {
-    const href = element.getAttribute('href');
-    
-    const objetoConfig = {
-        method: 'GET',
-        url: href
-    }
-
     try {
-        const response = await request(objetoConfig);
-        carregaResultado(response);
+        const href = element.getAttribute('href');
+        const response = await fetch(href);
+        
+        if(response.status !== 200) throw new Error('ERRO 404!');
+
+        const html = await response.text();
+        carregaResultado(html)
     } catch (e) {
         console.log(e);
     }
